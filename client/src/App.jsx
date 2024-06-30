d84bee0e19b9b56bfeebce26ef8b16bff82d53c0
Bhanujs18 <bhanupratap19995@gmail.com>
2024-06-30 15:49:29 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Class from './components/Class'
+import Home from './components/Home'
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        'og-color' : "#667BC6",
+      }
+    },
+  },
+  plugins: [],
+}
